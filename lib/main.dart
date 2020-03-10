16c2531a588fda60a32a3cc72193d6b09e3599e5
Kozhemyakin <agkozhemyakin@gmail.com>
2020-03-10 21:36:00 +0300

    caption theme style

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline1,
+              style: Theme.of(context).textTheme.caption,
             ),
             Text(
               'Revealed secret:\n${ConfigReader.getSecretKey()}',
